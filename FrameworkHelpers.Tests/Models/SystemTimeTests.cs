e0e650a556d1ef74bba82c1ee78957c1cef03dc4
Eddie Webb <edward.alexander-webb@gattacaplc.com>
2019-09-19 11:24:43 +0100

    FrameworkHelpers: Add - Base Test project and update solution files

---
+++ FrameworkHelpers.Tests/Models/SystemTimeTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FrameworkHelpers.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace FrameworkHelpers.Tests.Models
+{
+    [TestClass]
+    public class SystemTimeTests
+    {
+        [TestMethod]
+        public void When_overriding_now_it_should_return_overriden_value()
+        {
+            DateTime fix = new DateTime(2019, 9, 19,1,1,1);
+
+            SystemTime.OverrideWith(() => fix);
+
+            SystemTime.Now.ShouldBe(fix);
+        }
+    }
+}
---
+++ FrameworkHelpers.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FrameworkHelpers.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FrameworkHelpers.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f7d2c4df-99fa-464c-9d78-002fe6982d9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
