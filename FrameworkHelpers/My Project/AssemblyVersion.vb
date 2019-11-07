8cf65afe2ffc2cc3a55788810a16c020dd26a6f0
Eddie Webb <edward.alexander-webb@gattacaplc.com>
2019-11-07 12:48:47 +0000

    FrameworkHelpers: Add - Build files

---
+++ FrameworkHelpers/My Project/AssemblyVersion.vb
@@ -0,0 +1,7 @@
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+<assembly: AssemblyVersion("3.3.0.000")>
+<assembly: AssemblyFileVersion("3.3.0.000")>
+<assembly: AssemblyDescription("Build: 000")>
