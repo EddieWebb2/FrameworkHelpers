23f03ebbb2317733707c9430c8fb951c0ca8a368
Eddie Webb <edward.alexander-webb@gattacaplc.com>
2019-11-08 12:33:47 +0000

    Version update and release notes added

--- FrameworkHelpers/My Project/AssemblyVersion.vb
+++ FrameworkHelpers/My Project/AssemblyVersion.vb
@@ -2,6 +2,6 @@ Imports System.Reflection
 Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 
-<assembly: AssemblyVersion("3.3.0.000")>
-<assembly: AssemblyFileVersion("3.3.0.000")>
+<assembly: AssemblyVersion("1.0.0.000")>
+<assembly: AssemblyFileVersion("1.0.0.000")>
 <assembly: AssemblyDescription("Build: 000")>
--- ReleaseNotes.md
+++ ReleaseNotes.md
@@ -1,5 +1,8 @@
 # Release Notes - FrameworkHelpers
 
+## 1.1.0
+- Package now builds locally and is deploy ready.
+
 ## 0.1.0
 - Added App config helpers to support ease of use.
 
