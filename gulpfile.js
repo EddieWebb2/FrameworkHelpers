8f14220f0bfbd8ad4724d58445e312b7e7185cb9
Eddie Webb Private <eddiewebb2@hotmail.com>
2020-07-24 17:37:35 +0100

    Frameworkhelpers: Update - Gulpfile which wasnt correctly formated

--- gulpfile.js
+++ gulpfile.js
@@ -15,98 +15,102 @@ var debug = require("gulp-debug");
 var project = JSON.parse(fs.readFileSync("./package.json"));
 
 var config = {
-  name: project.name,
-  version: project.version,
-  mode: args.mode || "Debug",
-  output: ".build/deploy",
-  deployTarget: args.deployTarget,
-  releasenotesfile: "ReleaseNotes.md",
-  buildNumber: args.build || "000"
-  
+    name: project.name,
+    version: project.version,
+    mode: args.mode || "Debug",
+    output: ".build/deploy",
+    deployTarget: args.deployTarget,
+    buildNumber: args.BuildVersion || "000",
+    releasenotesfile: "ReleaseNotes.md"
 }
 
-gulp.task('default', [ "restore", "version", "compile", "test" ]);
+gulp.task('default', ["restore", "version", "compile", "test"]);
 
-gulp.task('restore', function() {
-  return gulp
-    .src(config.name + '.sln', { read: false })
-    .pipe(shell('".build/tools/nuget.exe" restore '));
-});
-
-gulp.task('version', function() {
-  return gulp
-    .src('.build/AssemblyVersion.base')
-    .pipe(rename("AssemblyVersion.vb"))
-    .pipe(assemblyInfo({
-      version: config.version,
-      fileVersion: config.version,
-      description: "Build: " +  config.buildNumber
-    }))
-    .pipe(gulp.dest(config.name + '/My Project'));
-});
-
-gulp.task('compile', [ "restore", "version" ], function() {
-  return gulp
-    .src(config.name + ".sln")
-    .pipe(msbuild({
-      targets: [ "Clean", "Rebuild" ],
-      configuration: config.mode,
-      toolsVersion: 14.0,
-      errorOnFail: true,
-      stdout: true,
-      verbosity: "minimal"
-    }));
-});
-
-gulp.task('test', [ "compile" ], function() {
-  return gulp
-    .src(['**/bin/*/*.Tests.dll'], { read: false })
-    .pipe(xunit({
-      executable: 'packages/xunit.runner.console.2.1.0/tools/xunit.console.exe',
-      options: {
-        nologo: true,
-        verbose: true,
-      }
-    }));
-});
-
-gulp.task('package', [ "test" ], shell.task([
-  ' ".build/tools/nuget.exe"' +
-  ' pack ' + config.name + '/' + config.name + '.csproj' +
-  ' -version ' + config.version +
-  ' -Prop Configuration=' + config.mode +
-  ' -OutputDirectory ' + config.output
+gulp.task('restore', shell.task([
+    '".build/tools/nuget.exe" restore -configFile nuget.config'
 ]));
 
-gulp.task('deploy', [ "package" ], function() {
-
-  return gulp
-    .src(config.output + '/*.' + config.version + '.nupkg')
-    .pipe(debug())
-    .pipe(nuget.push({
-      feed: 'http://192.168.100.110:9999/',
-      nuget: ".build/tools/nuget.exe",
-      apiKey: 'ThisIsAReallyStrongPassword123'
-    }));
+gulp.task('version', function () {
+    return gulp
+        .src('.build/AssemblyVersion.base')
+        .pipe(rename("AssemblyVersion.cs"))
+        .pipe(assemblyInfo({
+            version: config.version,
+            fileVersion: config.version,
+            description: "Build: " + config.buildNumber
+        }))
+        .pipe(gulp.dest(config.name + '/Properties'));
 });
 
-gulp.task('bump:patch', function() {
-  return gulp
-    .src("./package.json")
-    .pipe(bump({ type: "patch"}))
-    .pipe(gulp.dest('./'));
+gulp.task('compile', ["restore", "version"], function () {
+    return gulp
+        .src(config.name + ".sln")
+        .pipe(msbuild({
+            targets: ["Clean", "Rebuild"],
+            configuration: config.mode,
+            toolsVersion: 14.0,
+            errorOnFail: true,
+            stdout: true,
+            verbosity: "minimal",
+            architecture: 'x64',
+            nodeReuse: false,
+            nologo: false,
+            properties: {
+                RunOctoPack: config.pack,
+                VisualStudioVersion: 14.0,
+            }
+        }));
 });
 
-gulp.task('bump:minor', function() {
-  return gulp
-    .src("./package.json")
-    .pipe(bump({ type: "minor"}))
-    .pipe(gulp.dest('./'));
+gulp.task('test', ["compile"], function () {
+    return gulp
+        .src(['**/bin/*/*.Tests.dll'], { read: false })
+        .pipe(xunit({
+            executable: 'packages/xunit.runner.console.2.2.0/tools/xunit.console.exe',
+            options: {
+                nologo: true,
+                verbose: true,
+            }
+        }));
 });
 
-gulp.task('bump:major', function() {
-  return gulp
-    .src("./package.json")
-    .pipe(bump({ type: "major"}))
-    .pipe(gulp.dest('./'));
+gulp.task('package', ["test"], shell.task([
+    ' ".build/tools/nuget.exe"' +
+    ' pack ' + config.name + '/' + config.name + '.csproj' +
+    ' -version ' + config.version +
+    ' -Prop Configuration=' + config.mode +
+    ' -OutputDirectory ' + config.output
+]));
+
+gulp.task('deploy', ["package"], function () {
+
+    return gulp
+        .src(config.output + '/*.' + config.version + '.nupkg')
+        .pipe(debug())
+        .pipe(nuget.push({
+            source: 'http://srv-bld-1:9999/nuget',
+            nuget: ".build/tools/nuget.exe",
+            apiKey: 'ThisIsAReallyStrongPassword123'
+        }));
+});
+
+gulp.task('bump:patch', function () {
+    return gulp
+        .src("./package.json")
+        .pipe(bump({ type: "patch" }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:minor', function () {
+    return gulp
+        .src("./package.json")
+        .pipe(bump({ type: "minor" }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:major', function () {
+    return gulp
+        .src("./package.json")
+        .pipe(bump({ type: "major" }))
+        .pipe(gulp.dest('./'));
 });
